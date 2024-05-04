8343c914c16e63736c945af9bb5db556ed1d4549
Elias Obeid <elias.obeid9.02@gmail.com>
2024-05-04 15:21:47 +0300

    Added Review serializer.

---
+++ backend/review_system/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from user_auth.serializers import UserSerializer
+from library_system.serializers import BookSerializer
+from review_system.models import Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = "__all__"
+        model = Review
+
+    book = BookSerializer()
+    author = UserSerializer()
