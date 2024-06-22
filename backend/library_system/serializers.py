0ff546198db652ac693f53019054ec401bb3ded2
Elias Obeid <elias.obeid9.02@gmail.com>
2024-06-22 18:11:34 +0300

    Redesigned the serializers for library_system.
    - Redesigned the serializers for all the models in library_system.
    - Made the serializers use HyperlinkedModelSerializer to allow for
    easier access in the fronted code to each object from the URL directly.

--- backend/library_system/serializers.py
+++ backend/library_system/serializers.py
@@ -1,4 +1,67 @@
 from rest_framework import serializers
+from rest_framework.validators import ValidationError
 
 from library_system import models
-from user_auth.serializers import UserSerializer
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Author
+        fields = ("url", "full_name", "first_name", "last_name")
+        read_only_fields = ("url", "full_name")
+        write_only_fields = ("first_name", "last_name")
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = ("url", "name")
+        read_only_fields = ("url",)
+
+
+class PublicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Publication
+        fields = ("url", "name")
+        read_only_fields = ("url",)
+
+
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Book
+        fields = (
+            "url",
+            "full_tile",
+            "reviews_star_average",
+            "title",
+            "edition",
+            "isbn",
+            "summary",
+            "pages",
+            "authors",
+            "categories",
+            "publication",
+            "publish_date",
+            "language",
+        )
+        read_only_fields = ("url", "full_title", "reviews_star_average")
+        write_only_fields = ("title", "edition")
+
+
+class BookInstanceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.BookInstance
+        fields = ("url", "is_overdue", "book", "borrower", "due_date", "status")
+        read_only_fields = ("url", "is_overdue")
+
+    def validate_book(self, value):
+        if self.instance is not None:
+            raise ValidationError({"book": "this field is not updatable."})
+        return value
+
+
+class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Review
+        fields = ("url", "book", "reviewer", "stars", "review_text")
+        read_only_fields = ("url",)
